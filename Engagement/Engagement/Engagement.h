658438762d3e4c4c754f3d838286cc0e7e17ae2c
Zeeshan <mohammad.zeeshan@rebeltechnology.io>
2018-08-06 16:52:08 +0500

    - initial commit

---
+++ Engagement/Engagement/Engagement.h
@@ -0,0 +1,19 @@
+//
+//  Engagement.h
+//  Engagement
+//
+//  Created by Zeeshan on 8/6/18.
+//  Copyright © 2018 Zeeshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Engagement.
+FOUNDATION_EXPORT double EngagementVersionNumber;
+
+//! Project version string for Engagement.
+FOUNDATION_EXPORT const unsigned char EngagementVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Engagement/PublicHeader.h>
+
+
